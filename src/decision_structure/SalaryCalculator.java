88eb7c2b6dc1cca40c18f639dd32414554c6379f
steller520 <shivamnegi571@gmail.com>
2026-01-19 13:43:14 +0530

    feat: complete gross pay calculator class

---
+++ src/decision_structure/SalaryCalculator.java
@@ -0,0 +1,18 @@
+package decision_structure;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args) {
+
+        //Initialize known values
+        int salary = 1000;
+        int bonus = 250;
+        int quota = 10;
+
+        //Get values for the unkonwn
+
+
+    }
+}
